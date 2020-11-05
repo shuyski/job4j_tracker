b01cc8478ee809ec5c323c704b295f9c2cc2faaa
Руслан <ryslan.shuyski@mail.ru>
2020-11-05 11:48:54 +0300

    1.3. Вызов метода с аргументами. [#383833]

--- src/main/java/ru/job4j/tracker/oop/Jukebox.java
+++ src/main/java/ru/job4j/tracker/oop/Jukebox.java
@@ -3,18 +3,18 @@ package ru.job4j.tracker.oop;
 public class Jukebox {
     public void music(int position) {
      if (position == 1) {
-         System.out.println("Пусть бегут неуклюже\n" +
-                 "Пешеходы по лужам,\n" +
-                 "А вода по асфальту рекой.\n" +
-                 "И неясно прохожим\n" +
-                 "В этот день непогожий,\n" +
+         System.out.println("Пусть бегут неуклюже" + System.lineSeparator() +
+                 "Пешеходы по лужам," + System.lineSeparator() +
+                 "А вода по асфальту рекой." + System.lineSeparator() +
+                 "И неясно прохожим" + System.lineSeparator() +
+                 "В этот день непогожий," + System.lineSeparator() +
                  "Почему я веселый такой.");
      }
      else {
          if (position == 2) {
-             System.out.println("Крыши домов дрожат под тяжестью дней\n" +
-                     "Небесный пастух пасёт облака\n" +
-                     "Город стреляет в ночь дробью огней\n" +
+             System.out.println("Крыши домов дрожат под тяжестью дней" + System.lineSeparator() +
+                     "Небесный пастух пасёт облака" + System.lineSeparator() +
+                     "Город стреляет в ночь дробью огней" + System.lineSeparator() +
                      "Но ночь сильней, её власть велика.");
          }
          else   {
