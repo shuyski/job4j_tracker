6aa14973d484266122e1c7437a1f77629d500e64
Руслан <ryslan.shuyski@mail.ru>
2020-11-08 15:52:07 +0300

    6. Date. Отображение даты. [#383840]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private static LocalDateTime created;
+
+    public static LocalDateTime getCreated() {
+        created = LocalDateTime.now();
+        return created;
+    }
 
     public int getId() {
         return id;
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+
+public class StartUI {
+
+    public static void main(String[] args) {
+       DateTimeFormatter item = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+       String datatime = Item.getCreated().format(item);
+       System.out.println("Сегодня: " + datatime);
+    }
+}
