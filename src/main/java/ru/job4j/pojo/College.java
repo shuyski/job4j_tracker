e354fb632acb3b196b7fb4f4fbc35139d3e1e417
Руслан <ryslan.shuyski@mail.ru>
2020-11-14 13:56:47 +0300

    2. Модель данных. [#383851]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.pojo;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Шуйский Руслан, ");
+        student.setGroup(104);
+        student.setDate("01-09-2020 ");
+        System.out.println(student.getFio()
+                +"группа: " + student.getGroup() + ","
+                + " Дата зачисления: " + student.getDate()
+        );
+    }
+}
