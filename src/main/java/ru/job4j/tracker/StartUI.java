8b93803de1e3f0ee6ec5670b9f5a9bdf04900d49
Руслан <ryslan.shuyski@mail.ru>
2020-11-20 00:10:17 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#383785]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,12 +6,10 @@ public class StartUI {
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {this.showMenu();
-            //System.out.print("Select: ");
             int select = input.askInt("Select: ");
             switch (select) {
                 case 0 :
                 System.out.println("=== Create a new Item ====");
-                //System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
                 Item item = new Item(23, name);
                 tracker.add(item);
@@ -26,9 +24,7 @@ public class StartUI {
                     break;
                 case 2 :
                     int id;
-                    //System.out.println("Введите номер редактируемой заявки:");
                     id = input.askInt("Введите номер редактируемой заявки:");
-                    //System.out.println("Введите новое имя:");
                     String nov = input.askStr("Введите новое имя:");
                     item = new Item(id, nov);
                     if (!tracker.replace(id, item)) {
@@ -41,7 +37,6 @@ public class StartUI {
                     }
                     break;
                 case 3 :
-                    //System.out.println("Введите номер заявки на удаление:");
                     id = input.askInt("Введите номер заявки на удаление:");
                     if (!tracker.delete(id)) {
                         System.out.println("Номер заявки не найден");
@@ -53,7 +48,6 @@ public class StartUI {
                     }
                     break;
                 case 4 :
-                    //System.out.println("Введите номер заявки:");
                     id = input.askInt("Введите номер заявки:");
                     item = tracker.findById(id);
                     if (item == null) {
@@ -66,7 +60,6 @@ public class StartUI {
                     }
                     break;
                 case 5 :
-                    //System.out.println("Введите имя:");
                     name = input.askStr("Введите имя:");
                     names = tracker.findByName(name);
                     if (names.length == 0) {
