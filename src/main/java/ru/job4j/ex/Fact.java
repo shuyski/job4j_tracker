7025b954084efeabf7d0091f9c4de93fcb9f0503
Руслан <ryslan.shuyski@mail.ru>
2020-11-24 12:09:53 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#383799]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -6,14 +6,12 @@ public class Fact {
     }
 
     public int calc(int n) {
-        int rsl = 1;
         if (n < 0) {
            throw new IllegalArgumentException("n less than zero");
         }
-        else {
+        int rsl = 1;
         for (int index = 1; index <= n; index++) {
             rsl *= index;
-        }
         }
         return rsl;
     }
