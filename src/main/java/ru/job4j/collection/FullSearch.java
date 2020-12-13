750016511f91659ae85c25c45e277db981c516d4
Руслан <ryslan.shuyski@mail.ru>
2020-12-13 17:29:37 +0300

    1. Уникальные задачи. [#383771]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        return numbers;
+    }
+}
