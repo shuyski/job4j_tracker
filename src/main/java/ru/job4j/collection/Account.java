b47f3cb25cf67cbe987b425f27e855f5a3177992
Руслан <ryslan.shuyski@mail.ru>
2020-12-14 17:54:42 +0300

    3. Модели данных и HashSet. [#383773]

---
+++ src/main/java/ru/job4j/collection/Account.java
@@ -0,0 +1,37 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Account {
+    private String passport;
+    private String username;
+    private String deposite;
+
+    public Account(String passport, String username, String deposite) {
+        this.passport = passport;
+        this.username = username;
+        this.deposite = deposite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(passport, account.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "passport='" + passport + '\'' +
+                ", username='" + username + '\'' +
+                ", deposite='" + deposite + '\'' +
+                '}';
+    }
+}
