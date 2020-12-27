6c34ad46b3e2d84437ad95e0397800e32a24b954
Руслан <ryslan.shuyski@mail.ru>
2020-12-27 15:56:44 +0300

    2. Отсортировать департаменты [#383748]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] o1o = o1.split("/");
+        String[] o2o = o2.split("/");
+        return rsl;
+    }
+}
