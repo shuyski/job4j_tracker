a1f9b1d15728345679cff9764a4018b2529cbce8
Руслан <ryslan.shuyski@mail.ru>
2021-01-19 00:22:54 +0300

    3. Уникальность элементов и сортировка. [#383593]

--- src/main/java/ru/job4j/stream/turfirm/Profiles.java
+++ src/main/java/ru/job4j/stream/turfirm/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream.turfirm;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +8,9 @@ public class Profiles {
     public List<Address> collect(List<Profile> prof) {
         return prof.stream()
                 .map(ad -> ad.getAddress())
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
+
--- src/test/java/ru/job4j/stream/turfirm/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/turfirm/ProfilesTest.java
@@ -16,6 +16,10 @@ public class ProfilesTest {
     public void setAdr() {
         adr.add(new Address("Moscow", "Bygrova", 15, 6));
         adr.add(new Address("Sochi", "Mihalkova", 4, 9));
+        adr.add(new Address("Moscow", "Bygrova", 15, 6));
+        adr.add(new Address("Sochi", "Mihalkova", 4, 9));
+        adr.add(new Address("Novosibirsk", "Dyatlova", 8, 15));
+        adr.add(new Address("Samara", "Novogilova", 9, 1));
     }
 
     @Test
@@ -26,6 +30,23 @@ public class ProfilesTest {
         profile.add(prof);
         List<Address> expected = new ArrayList<>();
         expected.add(new Address("Moscow", "Bygrova", 15, 6));
+        assertThat(profiles.collect(profile), is(expected));
+    }
+
+    @Test
+    public  void listSort() {
+        Profiles profiles = new Profiles();
+        List<Profile> profile = new ArrayList<>();
+        Profile prof;
+        for (Address list : adr) {
+            prof = new Profile(list);
+            profile.add(prof);
+        }
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Bygrova", 15, 6));
+        expected.add(new Address("Novosibirsk", "Dyatlova", 8, 15));
+        expected.add(new Address("Samara", "Novogilova", 9, 1));
+        expected.add(new Address("Sochi", "Mihalkova", 4, 9));
         assertThat(profiles.collect(profile), is(expected));
     }
 }
