af4a2580b92aad7647b963392f5b81efcb57f089
Руслан <ryslan.shuyski@mail.ru>
2021-01-24 13:54:48 +0300

    1. Метод .of [#383602]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -9,41 +8,41 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenAll() {
-        List<String> input = Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> input = List.of("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
+            List<String> expect = List.of("K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void sortAsc() {
-        List<String> input = Arrays.asList("K1/SK1", "K1/SK1/SSK2", "K2/SK1", "K2/SK1/SSK1", "K1/SK2", "K2", "K1");
+        List<String> input = List.of("K1/SK1", "K1/SK1/SSK2", "K2/SK1", "K2/SK1/SSK1", "K1/SK2", "K2", "K1");
         Departments.sortAsc(input);
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK2", "K1/SK2", "K2", "K2/SK1", "K2/SK1/SSK1");
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK2", "K1/SK2", "K2", "K2/SK1", "K2/SK1/SSK1");
         assertThat(input, is(expect));
     }
 
     @Test
     public void sortDesc() {
-        List<String> input = Arrays.asList("K1/SK1", "K1/SK1/SSK2", "K2/SK1", "K2/SK1/SSK1", "K1/SK2", "K2", "K1", "K2/SK1/SSK2", "K1/SK1/SSK1");
+        List<String> input = List.of("K1/SK1", "K1/SK1/SSK2", "K2/SK1", "K2/SK1/SSK1", "K1/SK2", "K2", "K1", "K2/SK1/SSK2", "K1/SK1/SSK1");
         Departments.sortDesc(input);
-        List<String> expect = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
+        List<String> expect = List.of("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
         assertThat(input, is(expect));
     }
 }
--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,12 +11,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -28,13 +26,13 @@ public class NotifyAccountTest {
 
     @Test
     public void noSent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("123", "Petr Arsentev", "000001"),
                 new Account("135", "Ryslan", "13174589")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "000001"),
                         new Account("135", "Ryslan", "13174589")
                 )
