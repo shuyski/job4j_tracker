61cd9e38228e944ccdde8aaf65c409cda33b8a10
Руслан <ryslan.shuyski@mail.ru>
2021-02-01 07:22:02 +0300

    2. Аттестация [#383608]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,23 +1,29 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analyze {
 
     public static double averageScore(Stream<Pupil> stream) {
-
         return stream.flatMap(s -> Stream.of(s.getSubjects()))
-                .mapToInt(s -> s.get(0).getScore())   // Почему get(0)?
+                .mapToInt(s -> s.get(0).getScore())
                 .average()
                 .orElse(0);
     }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
-        return List.of();
+        return stream.map(v -> new Tuple(v.getName(), v.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0)))
+                .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+
         return List.of();
     }
 
